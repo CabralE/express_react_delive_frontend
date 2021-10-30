41537c39f594099f186a91d2cb49f2f918fa0951
ed <cabrale234@gmail.com>
2021-10-30 15:45:39 -0400

    frontend v2

--- src/App.js
+++ src/App.js
@@ -10,6 +10,10 @@ import About from "./pages/About";
 import Projects from "./pages/Projects";
 
 function App() {
+    // URL should have YOUR HEROKU URL for your backend, make sure you include the trailing slash
+
+  const URL = "http://localhost:4000/";
+
   return (
     <div className="App">
       <Header />
