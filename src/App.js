4a68f593858de6fcbe16747ad8ea2ac95bdef63b
ed <cabrale234@gmail.com>
2021-10-30 12:23:56 -0400

    frontend

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+
+// Import Components
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Route, Switch } from "react-router-dom";
+// IMPORT PAGES
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/projects">
+          <Projects URL={URL} />
+        </Route>
+        <Route path="/about">
+          <About URL={URL} />
+        </Route>
+      </Switch>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
