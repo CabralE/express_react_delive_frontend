c65f5b3d3aaa7e84137437f0e2c259b4e8e0c35a
ed <cabrale234@gmail.com>
2021-10-30 16:25:28 -0400

    updating index

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Route } from 'react-router';
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Route>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  </Route>
+
   document.getElementById('root')
 );
 
